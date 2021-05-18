02a14e19165619e2af6851031e1306cf09ab9799
Alexandre Vanier <alex.vanier@live.fr>
2021-05-18 09:32:32 -0400

    Etape 2

---
+++ alexandre-vanier-4w4/category-cours.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+
+get_header();
+?>
+	<main>
+		<?php if ( have_posts() ) : ?>
+			<?php
+			/* Start the Loop */
+            $precedent = "XXXXXX";
+			//global $tProprieté;
+			while ( have_posts() ) :
+				the_post();
+                convertirTableau($tPropriété);
+				//print_r($tPropriété);
+				if ($tPropriété['typeCours'] != $precedent): 
+					if ("XXXXXX" != $precedent)	: ?>
+						</section>
+					<?php endif; ?>
+					<section>
+				<?php endif;
+					get_template_part( 'template-parts/content', 'grille-cours' );
+					$precedent = $tPropriété['typeCours'];
+			endwhile;?>
+		<?php endif; ?>
+	</main><!-- #main -->
+
+<?php 
+// get_sidebar();
+get_footer();
+
+function convertirTableau(&$tPropriété)
+{
+	/*
+	$titre = get_the_title(); 
+	// 582-1W1 Mise en page Web (75h)
+	$sigle = substr($titre, 0, 7);
+	$nbHeure = substr($titre,-4,3);
+	$titrePartiel =substr($titre,8,-6);
+	$session = substr($titre, 4,1);
+	// $contenu = get_the_content();
+	// $resume = substr($contenu, 0, 200);
+	$typeCours = get_field('type_de_cours');
+*/
+
+	$tPropriété['titre'] = get_the_title(); 
+	$tPropriété['sigle'] = substr($tPropriété['titre'], 0, 7);
+	$tPropriété['nbHeure'] = substr($tPropriété['titre'],-4,3);
+	$tPropriété['titrePartiel'] = substr($tPropriété['titre'],8,-6);
+	$tPropriété['session'] = substr($tPropriété['titre'], 4,1);
+	$tPropriété['typeCours'] = get_field('type_de_cours');
+}
---
+++ alexandre-vanier-4w4/template-parts/content-grille-cours.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template part l'affichage des bloc de cours dans category-cours.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme4w4
+ */
+global $tPropriété;
+?>
+
+<article>
+			<p><?php echo $tPropriété['typeCours'];?></p>
+      <p><a href="<?php echo get_permalink() ?>"><?php echo $tPropriété['sigle'] ;?></a></p>
+      <p><?php echo $tPropriété['nbHeure'];?> </p>
+</article> 
